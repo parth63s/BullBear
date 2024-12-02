7c0ab27fa72a148374c4b0f8004c046f68df57cd
Parth Ramani <parthramani63@gmail.com>
2024-12-02 14:19:55 +0530

    Design Pricing-Page

--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-        <div className='container p-5 mb-5'>
+        <div className='container p-4 mb-5'>
             <div className="row text-center">
-                <h1 className='mt-5 mb-3'>Open a Zerodha account</h1>
-                <p className='mb-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+                <h1 className='mt-5 mb-3 fs-3'>Open a Zerodha account</h1>
+                <p className='mb-3 '>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
                 F&O trades.</p>
                 <button className="p-2 btn btn-primary fs-5" style={{width:"20%", margin:"0 auto"}}>Sign up Now</button>
             </div>
--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './Hero';
 import Brojkerage from './Brojkerage';
+import OpenAccount from '../OpenAccount.jsx'
 
 function PricingPage() {
     return ( 
         <>
             <Hero/>
+            <OpenAccount/>
             <Brojkerage/>
         </>
         
