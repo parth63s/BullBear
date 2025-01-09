747a8db3e5d1bb74cf4314f0d24e467a660ec2ee
Parth Ramani <parthramani63@gmail.com>
2025-01-09 15:19:03 +0530

    Active Link dashboard

--- Dashboard/src/components/Dashboard.jsx
+++ Dashboard/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Apps from "./Apps.jsx";
 import Funds from "./Funds.jsx";
@@ -17,14 +18,16 @@ const Dashboard = () => {
         <WatchList />
       {/* </GeneralContextProvider> */}
       <div className="content">
-        <Routes>
-          <Route exact path="/" element={<Summary/>} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/holdings" element={<Holdings />} />
-          <Route path="/positions" element={<Positions />} />
-          <Route path="/funds" element={<Funds />} />
-          <Route path="/apps" element={<Apps />} />
-        </Routes>
+          <Routes>
+            <Route exact path="/" element={<Summary/>} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/holdings" element={<Holdings />} />
+            <Route path="/positions" element={<Positions />} />
+            <Route path="/funds" element={<Funds />} />
+            <Route path="/apps" element={<Apps />} />
+          </Routes>
+        
+        
       </div>
     </div>
   );
--- Dashboard/src/components/Menu.jsx
+++ Dashboard/src/components/Menu.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import {Link} from "react-router-dom";
 
 const Menu = () => {
+  const [selectedMenu, setSelectedmenu] = useState(0);
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+
+  const handleMenuClick = (index) => {
+    setSelectedmenu(index);
+  }
+
+  
+  const handleProfileClick = (index) => {
+    setIsProfileDropdownOpen(!index);
+  }
+
+  const menuClass = "menu";
+  const activeMenuClass = "menu selected";
+
   return (
     <div className="menu-container">
       <div>
@@ -10,26 +26,41 @@ const Menu = () => {
       <div className="menus">
         <ul>
           <li>
-            <p>Dashboard</p>
+            <Link style={{textDecoration: "none"}} to="/" onClick={() => handleMenuClick(0)}>
+              <p className={selectedMenu == 0 ? activeMenuClass : menuClass}>Dashboard</p>
+            </Link>
           </li>
           <li>
-            <p>Orders</p>
+            <Link style={{textDecoration: "none"}} to="/Orders" onClick={() => handleMenuClick(1)}>
+              <p className={selectedMenu == 1 ? activeMenuClass : menuClass}>Orders</p>
+            </Link>
           </li>
           <li>
-            <p>Holdings</p>
+            <Link style={{textDecoration: "none"}} to="/Holdings" onClick={() => handleMenuClick(2)}>
+              <p className={selectedMenu == 2 ? activeMenuClass : menuClass}>Holdings</p>
+            </Link>
+            
           </li>
           <li>
-            <p>Positions</p>
+            <Link style={{textDecoration: "none"}} to="/Positions" onClick={() => handleMenuClick(3)}>
+              <p className={selectedMenu == 3 ? activeMenuClass : menuClass}>Positions</p>
+            </Link>
+            
           </li>
           <li>
-            <p>Funds</p>
+            <Link style={{textDecoration: "none"}} to="/funds" onClick={() => handleMenuClick(4)}>
+              <p className={selectedMenu == 4 ? activeMenuClass : menuClass}>Funds</p>
+            </Link>
           </li>
           <li>
-            <p>Apps</p>
+            <Link style={{textDecoration: "none"}} to="/apps" onClick={() => handleMenuClick(5)}>
+              <p className={selectedMenu == 5 ? activeMenuClass : menuClass}>Apps</p>
+            </Link>
+            
           </li>
         </ul>
         <hr />
-        <div className="profile" >
+        <div className="profile" onClick={handleProfileClick}>
           <div className="avatar">BU</div>
           <div className="username">
             <p>USERID</p>
