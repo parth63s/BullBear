9af79c9f71a81ac6941082cc0ea1e1e772d2b034
Parth Ramani <parthramani63@gmail.com>
2025-02-27 23:34:16 +0530

    Graph added in Dashboard

--- Dashboard/src/components/BuyStock.jsx
+++ Dashboard/src/components/BuyStock.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { Link } from "react-router-dom";
 
 import {GeneralContext} from "./GeneralContext";
@@ -67,5 +67,6 @@ const BuyActionWindow = ({ uid }) => {
     </div>
   );
 };
+
 
 export default BuyActionWindow;
--- Dashboard/src/components/Holdings.jsx
+++ Dashboard/src/components/Holdings.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import { VerticalGraph } from "./Charts/VerticalGraph";
 
 const Holdings = () => {
 
@@ -10,9 +11,44 @@ const Holdings = () => {
       setAllHoldings(res.data);
     })
   }, []);
+  
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = allHoldings.map((subArray)=>subArray["name"]);
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Stock Price',
+      data: allHoldings.map((stock) => (stock.price)),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+   
+  ]
+}
+
   return (
     <>
       <h3 className="title">Holdings ({allHoldings.length})</h3>
+      <div className="row">
+        <div className="col">
+          <h5>
+            29,875.<span>55</span>{" "}
+          </h5>
+          <p>Total investment</p>
+        </div>
+        <div className="col">
+          <h5>
+            31,428.<span>95</span>{" "}
+          </h5>
+          <p>Current value</p>
+        </div>
+        <div className="col">
+          <h5>1,553.40 (+5.20%)</h5>
+          <p>P&L</p>
+        </div>
+      </div>
+      <VerticalGraph data={data}/>
 
       <div className="order-table">
         <table>
@@ -40,7 +76,7 @@ const Holdings = () => {
                 <td>{stock.avg.toFixed(2)}</td>
                 <td>{stock.price.toFixed(2)}</td>
                 <td>{curValue.toFixed(2)}</td>
-                <td className={profClass}>{isProfit}</td>
+                <td className={profClass}>{(curValue - stock.avg * stock.qty).toFixed(2)}</td>
                 <td className={profClass}>{stock.net}</td>
                 <td className={dayClass}>{stock.day}</td>
               </tr>
@@ -50,24 +86,6 @@ const Holdings = () => {
         </table>
       </div>
 
-      <div className="row">
-        <div className="col">
-          <h5>
-            29,875.<span>55</span>{" "}
-          </h5>
-          <p>Total investment</p>
-        </div>
-        <div className="col">
-          <h5>
-            31,428.<span>95</span>{" "}
-          </h5>
-          <p>Current value</p>
-        </div>
-        <div className="col">
-          <h5>1,553.40 (+5.20%)</h5>
-          <p>P&L</p>
-        </div>
-      </div>
     </>
   );
 };
